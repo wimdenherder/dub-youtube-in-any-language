2f7f0ef35b8a1da6c340379e9334748d80aaae68
wimdenherder <wimdenherder@gmail.com>
2023-03-21 15:23:30 +0100

    Write shorter

--- dubbify-shortest.js
+++ dubbify-shortest.js
@@ -1,26 +1,22 @@
 let lastIndex = -1, lang = "nl", utterance, subs = await getSubs(lang);
 async function getSubs(langCode) {
-  const ct = ytInitialPlayerResponse.captions.playerCaptionsTracklistRenderer.captionTracks;
-  const subsUrl = (ct.find(x => x.vssId.indexOf("." + langCode) === 0)?.baseUrl || ct.find(x => x.vssId.indexOf("a." + langCode) === 0)?.baseUrl || ct[0].baseUrl + "&tlang=" + langCode ) + "&fmt=json3";
+  const subsUrl = ((ct = ytInitialPlayerResponse.captions.playerCaptionsTracklistRenderer.captionTracks).find(x => x.vssId.indexOf("." + langCode) === 0)?.baseUrl || ct.find(x => x.vssId.indexOf("a." + langCode) === 0)?.baseUrl || ct[0].baseUrl + "&tlang=" + langCode ) + "&fmt=json3";
   return (await (await fetch(url)).json()).events.map(x => ({...x, text: x.segs?.map(x => x.utf8)?.join(" ")?.replace(/\n/g,' ')?.replace(/♪|'|"|\.{2,}|\<[\s\S]*?\>|\{[\s\S]*?\}|\[[\s\S]*?\]/g,'')?.trim() || ''}));
 }
-function speak(text, lang, dur) {
+function speak(text, lang) {
     let utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = lang
     speechSynthesis.speak(utterance);
     utterance.onend = () => utterance = false;
 }
 async function speakCurrentSub() {
-  const v = document.querySelector('video');
-  const currentIndex = subs.findIndex(x => x.start <= v.currentTime && x.start + x.dur >= v.currentTime);
+  const vid = document.querySelector('video');
+  const currentIndex = subs.findIndex(x => x.start <= vid.currentTime && x.start + x.dur >= vid.currentTime);
   if (currentIndex === -1 || lastIndex === currentIndex) return;
-  if (utterance) { 
-    v.pause();
-    return setTimeout(speakCurrentSub, 100);
-  }
-  v.play();
-  lastIndex = currentIndex;
-  speak(subs[currentIndex].text, lang, subs[currentIndex].dur);
+  if (utterance)
+    return setTimeout(speakCurrentSub, 100) && vid.pause();
+  vid.play();
+  speak(subs[(lastIndex = currentIndex)].text, lang);
 }
 setInterval(speakCurrentSub, 10);
 
