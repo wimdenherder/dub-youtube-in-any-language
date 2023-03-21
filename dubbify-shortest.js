20beca7a78c41be6c4b52928ad2d03c5055b2661
wimdenherder <wimdenherder@gmail.com>
2023-03-21 15:16:22 +0100

    Replace global var speaking with utterance

--- dubbify-shortest.js
+++ dubbify-shortest.js
@@ -1,21 +1,20 @@
-let lastIndex = -1, lang = "nl", speaking = false, subs = await getSubs(lang);
+let lastIndex = -1, lang = "nl", utterance, subs = await getSubs(lang);
 async function getSubs(langCode) {
   const ct = ytInitialPlayerResponse.captions.playerCaptionsTracklistRenderer.captionTracks;
   const subsUrl = (ct.find(x => x.vssId.indexOf("." + langCode) === 0)?.baseUrl || ct.find(x => x.vssId.indexOf("a." + langCode) === 0)?.baseUrl || ct[0].baseUrl + "&tlang=" + langCode ) + "&fmt=json3";
   return (await (await fetch(url)).json()).events.map(x => ({...x, text: x.segs?.map(x => x.utf8)?.join(" ")?.replace(/\n/g,' ')?.replace(/♪|'|"|\.{2,}|\<[\s\S]*?\>|\{[\s\S]*?\}|\[[\s\S]*?\]/g,'')?.trim() || ''}));
 }
 function speak(text, lang, dur) {
-    speaking = true
     let utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = lang
     speechSynthesis.speak(utterance);
-    utterance.onend = () => speaking = false;
+    utterance.onend = () => utterance = false;
 }
 async function speakCurrentSub() {
   const v = document.querySelector('video');
   const currentIndex = subs.findIndex(x => x.start <= v.currentTime && x.start + x.dur >= v.currentTime);
   if (currentIndex === -1 || lastIndex === currentIndex) return;
-  if (speaking) { 
+  if (utterance) { 
     v.pause();
     return setTimeout(speakCurrentSub, 100);
   }
