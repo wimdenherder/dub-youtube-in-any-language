6761d7733f5d60d2823936110b482793fc4c6c85
wimdenherder <wimdenherder@gmail.com>
2023-03-22 22:15:55 +0100

    Fix typo in getting subtitle url

--- dubbify.js
+++ dubbify.js
@@ -1,8 +1,8 @@
-const fetchYTData = async () => { try { return JSON.parse((await (await fetch(window.location.href)).text()).split(`ytInitialPlayerResponse = `)[1].split(`;var`)[0]); } catch (e) {}};
-let lastIndex = -1, lang = "ru", voice, vid = document.querySelector('video'), ct = (ytInitialPlayerResponse || await fetchYTData()).captions.playerCaptionsTracklistRenderer.captionTracks, subs = await getSubs(lang), baseVolume = vid.volume;
+const fetchYTData = async () => { try { return JSON.parse((await (await fetch(window.location.href)).text()).split('ytInitialPlayerResponse = ')[1].split(';var')[0]); } catch (e) {}};
+let lastIndex = -1, lang = "ar", voice, vid = document.querySelector('video'), ct = (ytInitialPlayerResponse || await fetchYTData()).captions.playerCaptionsTracklistRenderer.captionTracks, subs = await getSubs(lang), baseVolume = vid.volume;
 async function getSubs(langCode) {
   const findCaptionUrl = x => ct.find(y => y.vssId.indexOf(x) === 0)?.baseUrl;
-  const url = (findCaptionUrl("." + langCode) || findCaptionUrl(".") + "&tlang=" + langCode || findCaptionUrl("a." + langCode) || ct[0].baseUrl + "&tlang=" + langCode) + "&fmt=json3"
+  const url = (findCaptionUrl("." + langCode) || findCaptionUrl(".") || findCaptionUrl("a." + langCode) || ct[0].baseUrl + "&tlang=" + langCode) + "&fmt=json3"
   return (await (await fetch(url)).json()).events.map(x => ({...x, text: x.segs?.map(x => x.utf8)?.join(" ")?.replace(/\n/g,' ')?.replace(/♪|'|"|\.{2,}|\<[\s\S]*?\>|\{[\s\S]*?\}|\[[\s\S]*?\]/g,'')?.trim() || ''}));
 }
 async function speak() {
